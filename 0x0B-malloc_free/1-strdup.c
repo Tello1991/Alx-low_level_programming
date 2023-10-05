c6048113203993cd0aa317f8ae58c695bc1bda61
Tello1991 <miyatellopilot@gmail.com>
2023-10-04 20:28:19 -0700

    first commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Entry point
+ *@str: string we need to duplicate
+ * Return: a pointer to the duplicated string or NULL
+ */
+char *_strdup(char *str)
+{
+	char *strnew = NULL;
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - concatenates all the arguments of your program
+ *@str: string
+ *@av: arguments
+ * Return: a pointer to a new string
+ */
+char **strtow(char *str)
+{
